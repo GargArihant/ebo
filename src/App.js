6c38c35fe24498e028d83a7a3a51db80970b4e44
arihant <arihantgarg2002@gmail.com>
2023-04-23 18:23:56 +0530

    sixth commit

--- src/App.js
+++ src/App.js
@@ -2,39 +2,39 @@ import './App.css';
 
 function App() { 
   return (
-  <div className="App">
-   <div class="container">
+  <div classNameName="App">
+   <div className="container">
         <header></header>
-        <div class="main">
-            <div class="left">
-                <div class="cart">
-                    <div class="cart_icon">
-                        <img class="left_arrow" src=".\scroll_left.svg.svg" alt="" ></img>
+        <div className="main">
+            <div className="left">
+                <div className="cart">
+                    <div className="cart_icon">
+                        <img className="left_arrow" src=".\scroll_left.svg.svg" alt="" ></img>
                     </div>
-                    <div class="cart_message">
+                    <div className="cart_message">
                         <p><span>Cart</span> (2 items)</p>
                     </div>
                 </div>
-                <div class="savings">
+                <div className="savings">
                     <p>Your total savings</p>
                     <p>&#8377;750</p>
                 </div>
-                <div class="celeb_container">
-                    <div class="celeb">
+                <div className="celeb_container">
+                    <div className="celeb">
                         <p>Who's celebrating?</p>
                         <p>Planning to surprise someone click on 'someone else'</p>
                       </div>
-                      <div class="celeb_buttons">
+                      <div className="celeb_buttons">
                         <button><p>Myself <img src=".\pen.svg.svg" alt=""></img></p></button>
                         <button><p>Someone else</p></button>
                       </div>
                 </div>
-                <div class="card_container">
-                    <div class="card">
+                <div className="card_container">
+                    <div className="card">
                         <img src=".\balloon.svg" alt=""></img>
-                        <div class="card_details">
-                          <div class = "card_details_header"> 
-                            <div class="card_title">
+                        <div className="card_details">
+                          <div className = "card_details_header"> 
+                            <div className="card_title">
                               <p>Purple Anniversory Decor with Occasional lighting</p>
                             </div>
                             <img src=".\delete.svg.svg" alt=""></img>
@@ -49,18 +49,18 @@ function App() {
                           </div>
                         </div>
                       </div>
-                      <div class="card_footer">
+                      <div className="card_footer">
                         <p>Add Ons
-                            <div class="one_img">
+                            <div className="one_img">
                                 <img src=".\one.svg" alt=""></img>
                             </div> 
                         </p> 
                         <p>Add more items</p>
                       </div>
                 </div>
-                <div class="card2_container">
-                    <div class="card2_title">
-                        <img class="face" src=".\noto_partying-face.svg" alt=""></img>
+                <div className="card2_container">
+                    <div className="card2_title">
+                        <img className="face" src=".\noto_partying-face.svg" alt=""></img>
                         <p>Party Add one</p>
                       </div>
                       <div>
@@ -69,99 +69,99 @@ function App() {
                         <button>Occassional Foils</button>
                         <button>More Add ons</button>
                       </div>
-                      <div class="list">
-                        <div class="list_display" >
+                      <div className="list">
+                        <div className="list_display" >
                           <img src=".\C553E9DE-4D55-4F8C-822E-586A7AF17D76 (1).svg" alt=""></img>
                           <p>50 Additional Balloons</p>
                           <p>Matching to the decor theme</p>
-                          <div class="price">
+                          <div className="price">
                             <div> 
                               <p>&#8377;100</p>
                             </div>
-                            <div class="Add">Add
+                            <div className="Add">Add
                             </div>
                           </div>
                         </div>
                       
-                      <div class="list_display" >
+                      <div className="list_display" >
                         <img src=".\C553E9DE-4D55-4F8C-822E-586A7AF17D76 (1).svg" alt=""></img>
                         <p>50 Additional Balloons</p>
                         <p>Matching to the decor theme</p>
-                        <div class="price">
+                        <div className="price">
                           <div> 
                             <p>&#8377;100</p>
                           </div>
-                          <div class="Add">Add
+                          <div className="Add">Add
                           </div>
                         </div>
                       </div>
-                      <div class="list_display">
+                      <div className="list_display">
                         <img src=".\C553E9DE-4D55-4F8C-822E-586A7AF17D76 (1).svg" alt=""></img>
                         <p>50 Additional Balloons</p>
                         <p>Matching to the decor theme</p>
-                        <div class="price">
+                        <div className="price">
                           <div> <p>&#8377;100</p>
                           </div>
-                          <div class="Add">Add
+                          <div className="Add">Add
                           </div>
                         </div>
                       </div>
                     </div>
                 </div>
             </div>
-            <div class="right">
-                <div class="gift_container">
-                    <div class="offer">
+            <div className="right">
+                <div className="gift_container">
+                    <div className="offer">
                         <div>
                             <p>Unlock your free gift!</p>
                             <p>Add worth &#8377;432 more to unlock!</p>
                         </div>
                         <img src=".\Group.svg" alt=""></img>
                     </div>
-                    <div class="offer_pic">
+                    <div className="offer_pic">
                         <img src=".\E203D906-EE2F-4F8F-B445-820A017FCAD2 (1).svg" alt=""></img>
                         <div>
                             <p>Chocolate brownie</p>
                             <p>Free</p>
                         </div>
-                        <div class="locked">
+                        <div className="locked">
                             <img src=".\lock.svg.svg" alt=""></img>
                             <p>Locked</p>
                         </div>
                         <img src=".\Vector.svg" alt=""></img>
                     </div> 
                 </div>
-                <div class="pay_container">
+                <div className="pay_container">
                     <div><p>Cart total: <span>&#8377;2500</span> (2 items)</p></div>
-        <div class="empty"></div>
+        <div className="empty"></div>
         <div >
-            <div class="line">
-              <div class="line_icon">
+            <div className="line">
+              <div className="line_icon">
               <img src=".\percentage.svg" alt=""></img>
             <p>Save &#8377;500 more on this boking</p></div>
-            <div class="change" >Apply</div>
+            <div className="change" >Apply</div>
         </div>
         <div>
             <p>Code: EBONOW</p>
         </div>
         <div><p>View all coupons</p></div>  
         </div>
-        <div class="line" > 
-            <div class="line_icon">
+        <div className="line" > 
+            <div className="line_icon">
               <img src=".\home.svg" alt=""></img>
             <p><span>Home</span> -Raj villa, Ashram Para, Silli ...</p></div>
             <div>Change</div>
         </div>
-        <div class="line">
-            <div class = "line_icon">
+        <div className="line">
+            <div className = "line_icon">
               <img src=".\cart_done.svg" alt=""></img>
             <p><span>Fri Jan 27</span> 7:00 - 9:00pm</p></div>
-            <div class="change">Change</div></div>
+            <div className="change">Change</div></div>
         <div> <p>
             Proceed to Pay
         </p></div>
                 </div>
-                <div class="bill">
+                <div className="bill">
                     <p>Bill summary</p>
                     <p>Base value  &#8377;1000</p>
                     <p>Add Ons &#8377;200</p>
